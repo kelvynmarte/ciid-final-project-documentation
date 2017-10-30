17a42411fad9dbd4f360007249a2bbdccef0dfb4
Kelvyn Ornette Sol Marte <kelvyn@kelvyn.net>
2017-10-30 14:05:35 +0100

    Update Prototyping Content

--- src/components/SiteNavi/index.jsx
+++ src/components/SiteNavi/index.jsx
@@ -14,60 +14,76 @@ class SiteNavi extends React.Component {
               </a>
             </h1>
             <p className="lead">
-              CIID final project documentation of <a href="http://ciid.dk/education/people/students-2017/kelvyn-marte/" target="_blank">Kelvyn&nbsp;Marte</a>.
+              CIID final project documentation of{' '}
+              <a
+                href="http://ciid.dk/education/people/students-2017/kelvyn-marte/"
+                target="_blank"
+              >
+                Kelvyn&nbsp;Marte
+              </a>.
             </p>
           </div>
 
           <nav className="sidebar-nav">
-
-              <Link
-                to="/introduction/"
-                className={
-                  location.pathname.endsWith('/introduction/')
+            <Link
+              to="/introduction/"
+              className={
+                location.pathname.endsWith('/introduction/')
                   ? 'sidebar-nav-item active'
                   : 'sidebar-nav-item'
-                }>Introduction</Link>
+              }
+            >
+              Introduction
+            </Link>
 
-                <Link
-                  to="/research/"
-                  className={
-                    location.pathname.endsWith('/research/')
-                    ? 'sidebar-nav-item active'
-                    : 'sidebar-nav-item'
-                  }>Research</Link>
+            <Link
+              to="/research/"
+              className={
+                location.pathname.endsWith('/research/')
+                  ? 'sidebar-nav-item active'
+                  : 'sidebar-nav-item'
+              }
+            >
+              Research
+            </Link>
 
-                <Link
-                  to="/opportunityareas/"
-                  className={
-                    location.pathname.endsWith('/opportunityareas/')
-                    ? 'sidebar-nav-item active'
-                    : 'sidebar-nav-item'
-                  }>Opportunity Areas</Link>
+            <Link
+              to="/opportunityareas/"
+              className={
+                location.pathname.endsWith('/opportunityareas/')
+                  ? 'sidebar-nav-item active'
+                  : 'sidebar-nav-item'
+              }
+            >
+              Opportunity Areas
+            </Link>
 
-                  <Link
-                    to="/concepting/"
-                    className={
-                      location.pathname.endsWith('/concepting/')
-                      ? 'sidebar-nav-item active'
-                      : 'sidebar-nav-item'
-                    }>Concepting</Link>
+            <Link
+              to="/concepting/"
+              className={
+                location.pathname.endsWith('/concepting/')
+                  ? 'sidebar-nav-item active'
+                  : 'sidebar-nav-item'
+              }
+            >
+              Concepting
+            </Link>
 
-                    <Link
-                      to="/prototyping/"
-                      className={
-                        location.pathname.endsWith('/prototyping/')
-                        ? 'sidebar-nav-item active'
-                        : 'sidebar-nav-item'
-                      }>Prototyping</Link>
+            <Link
+              to="/prototyping/"
+              className={
+                location.pathname.endsWith('/prototyping/')
+                  ? 'sidebar-nav-item active'
+                  : 'sidebar-nav-item'
+              }
+            >
+              Prototyping
+            </Link>
+          </nav>
+        </div>
+      </div>
+    )
+  }
+}
 
-
-                    </nav>
-                  </div>
-                </div>
-
-
-              )
-            }
-          }
-
-          export default SiteNavi
+export default SiteNavi
--- src/pages/prototyping/index.md
+++ src/pages/prototyping/index.md
@@ -10,16 +10,36 @@ categories:
 description: ""
 ---
 
-## Experiment 1
+## Experiment I
+
+### Insights
+
+**The perception of Nørrebro changed a lot after the shooting.**
+
+>The shooting the last months changed the place a lot, the police is now constantly at the places and people are of course aware of it.
+><footer><cite>Gustav, 2017</cite></footer>
+
+**There are a lot of different attempts to create harmony in the diverse population of Nørrebro.**
+
+>After the shooting everyone came here to backup and say that this is a good place. that took my heart. People in the community certainly got closer together. We protest silently against the violence happening with cake and donuts.
+><footer><cite>Marie, 2017</cite></footer>
+
+>There is a hidden city behind the facades of Porto — talking about the islands (ihlas) in the courtyards of Porto, where the poor working class was and is living.
+><footer><cite>João, 2017</cite></footer>
 
 ### Idea
 
-This prototype is an exhibition in the urban space, where *visitors* of the exhibition could experience the rich diverse events, but also just daily activities of other people living in the same neighborhood.
-The idea is inspired by the recent events happening in Nørrebro as response to the shooting, where people show their support for the neighborhood and more communal activities are happening now than before. One of the efforts made is also to fight the perception that the area is a dangerous place. Particular Nørrebro has a very diverse population and activities happening, so it will be particular interesting for citizens living there but also visitors to have a glimpse into other peoples activities.
+This prototype is an exhibition in the urban space, where *visitors* of the exhibition could experience the rich diverse events, but also just daily activities of other people living in the same neighbourhood. The idea is inspired by the recent events happening in Nørrebro as response to the shooting, where people show their support for the neighbourhood and more communal activities are happening now than before. One of the efforts made is also to fight the perception that the area is a dangerous place. Particular Nørrebro has a very diverse population and activities happening, so it will be particular interesting for citizens living there but also visitors to have a glimpse into other peoples activities.
 
 ![Sketch of Experiment 1](experiment1–sketch1.jpg)
 
 *The idea is that there will be spheres added to the space by AR, when people walk closer they can have a audio peak into them. When they walk into the sphere it will they can see a 360° video of a activity or event happening at the same place*
+
+### Prototyping Questions
+
+* How is to experience AR through the phone, for a longer time?
+* Is 360° video a valid way to deliver intimate experiences?
+* Can I build a AR prototype in a decent time?
 
 ### Prototyping
 
@@ -41,6 +61,7 @@ Due to a personally set time constraint for the first prototype, I tested the fi
 * Key experiences are approaching and stepping into the sphere
 * There is a bit of discomfort looking at the screen
 * 360° video on a phone does not feel very intimate if you are not very close due to the broad field of view.
+* Prototyping AR applications with current frameworks is quite fast.
 
 >In general there is a bit of discomfort, walking around and just looking at the screen
 ><footer><cite>Matt</cite></footer>
